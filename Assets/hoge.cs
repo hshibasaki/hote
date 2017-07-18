eef01f122acf1714c9c43ab730bb1f57bdc26981
hshibasaki <shibasaki1988@gmail.com>
2017-07-19 07:47:52 +0900

    add code

---
+++ Assets/hoge.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //追加
+
+
+public class hoge : MonoBehaviour {
+
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello Github!";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
